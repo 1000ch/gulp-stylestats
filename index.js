d208132b4e38df3c8d43bfa8a0ef801d2b3fa176
1000ch <shogo.sensui@gmail.com>
2014-04-11 09:37:35 +0900

    refactoring

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 
 var Table = require('cli-table');
 var numeral = require('numeral');
+var moment = require('moment');
 var json2csv = require('json2csv');
-var _ = require('underscore');
-_.str = require('underscore.string');
-_.mixin(_.str.exports());
-_.str.include('Underscore.string', 'string');
 var gutil = require('gulp-util');
 var through = require('through2');
+
 var StyleStats = require('stylestats');
+var aliases = require('./node_modules/stylestats/assets/aliases.json');
 
 module.exports = function (options) {
 
   options = options || {};
 
   return through.obj(function (file, encode, callback) {
+
     if (file.isNull()) {
       this.push(file);
       return callback();
     }
+
     if (file.isStream()) {
       this.emit('error', new gutil.PluginError('gulp-stylestats', 'Streaming not supported'));
       return callback();
@@ -84,23 +85,29 @@ function prettify(result) {
   var collections = [];
   Object.keys(result).forEach(function(key) {
     var stats = {};
-    var prop = _(_(key).humanize()).titleize();
+    stats.name = key;
+    stats.prop = aliases[key];
     if (key === 'propertiesCount') {
       var array = [];
       result[key].forEach(function(item) {
         array.push([item.property, item.count]);
       });
-      stats[prop] = array.join('\n').replace(/\,/g, ': ');
+      stats.value = array.join('<br>').replace(/\,/g, ': ');
+    } else if (key === 'published') {
+      stats.value = moment().format('lll');
     } else if (key === 'size' || key === 'gzippedSize' || key === 'dataUriSize') {
-      stats[prop] = numeral(result[key]).format('0.0b').replace(/\.0B/, 'B').replace(/0\.0/, '0');
+      stats.value = numeral(result[key]).format('0.0b').replace(/\.0B/, 'B').replace(/0\.0/, '0');
     } else if (key === 'simplicity' || key === 'ratioOfDataUriSize') {
-      stats[prop] = numeral(result[key]).format('0.00%');
-    } else if (key === 'published' || key === 'paths') {
-      return true;
+      stats.value = numeral(result[key]).format('0.0%');
+    } else if (key === 'uniqueColor') {
+      stats.value = result[key];
+      if (stats.value == '') {
+        stats.value = 'N/A';
+      }
     } else {
-      stats[prop] = Array.isArray(result[key]) ? result[key].join('\n') : result[key];
-      if (stats[prop] === '') {
-        stats[prop] = 'N/A';
+      stats.value = Array.isArray(result[key]) ? result[key].join('<br>') : result[key];
+      if (stats.value === '') {
+        stats.value = 'N/A';
       }
     }
     collections.push(stats);
