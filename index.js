0e026fc62da4ae9d1a5bff65cf3e6b575c157fb2
1000ch <shogo.sensui@gmail.com>
2014-09-22 16:01:44 +0900

    fix broken output

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 'use strict';
 
 var Table = require('cli-table');
-var numeral = require('numeral');
-var moment = require('moment');
 var json2csv = require('json2csv');
 var gutil = require('gulp-util');
 var through = require('through2');
 
 var StyleStats = require('stylestats');
-var aliases = require('./node_modules/stylestats/assets/aliases.json');
+var prettify = require('stylestats/lib/prettify');
 
 module.exports = function (options) {
 
@@ -63,7 +61,7 @@ module.exports = function (options) {
             }
           });
           prettify(result).forEach(function(data) {
-            table.push([data.prop, data.value]);
+            table.push(data);
           });
           console.log(' StyleStats!\n' + table.toString());
           break;
@@ -75,42 +73,3 @@ module.exports = function (options) {
     callback();
   });
 };
-
-/**
- * Prettify StyleStats data.
- * @param {object} [result] StyleStats parse data. Required.
- * @return {array} prettified data.
- */
-function prettify(result) {
-  var collections = [];
-  Object.keys(result).forEach(function(key) {
-    var stats = {};
-    stats.name = key;
-    stats.prop = aliases[key];
-    if (key === 'propertiesCount') {
-      var array = [];
-      result[key].forEach(function(item) {
-        array.push([item.property, item.count]);
-      });
-      stats.value = array.join('<br>').replace(/\,/g, ': ');
-    } else if (key === 'published') {
-      stats.value = moment().format('lll');
-    } else if (key === 'size' || key === 'gzippedSize' || key === 'dataUriSize') {
-      stats.value = numeral(result[key]).format('0.0b').replace(/\.0B/, 'B').replace(/0\.0/, '0');
-    } else if (key === 'simplicity' || key === 'ratioOfDataUriSize') {
-      stats.value = numeral(result[key]).format('0.0%');
-    } else if (key === 'uniqueColor') {
-      stats.value = result[key];
-      if (stats.value == '') {
-        stats.value = 'N/A';
-      }
-    } else {
-      stats.value = Array.isArray(result[key]) ? result[key].join('<br>') : result[key];
-      if (stats.value === '') {
-        stats.value = 'N/A';
-      }
-    }
-    collections.push(stats);
-  });
-  return collections;
-}
