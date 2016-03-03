cfd16329cda030b7840990a2b70057d50f444923
1000ch <shogo.sensui@gmail.com>
2016-03-03 19:38:53 +0900

    remove unused files

--- test/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var stylestats = require('../');
-
-gulp.task('stylestats', function () {
-  gulp.src('./fixtures/*.css')
-    .pipe(stylestats());
-});
-
-gulp.task('default', ['stylestats']);
